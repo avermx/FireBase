f657d57fc5ff845d57c569d4dc89b2e589b4234d
avermx <aniket125verma@gmail.com>
2025-08-29 22:29:28 +0530

    init

--- vite-project/src/Firebase.js
+++ vite-project/src/Firebase.js
@@ -8,10 +8,11 @@ const firebaseConfig = {
   storageBucket: "todo-6d23d.firebasestorage.app",
   messagingSenderId: "76855814933",
   appId: "1:76855814933:web:cde56e3fc02638cb4e171c",
-  measurementId: "G-2TC0SY9M37"
+  measurementId: "G-2TC0SY9M37",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 export default app;
--- vite-project/src/components/Dashboard.jsx
+++ vite-project/src/components/Dashboard.jsx
@@ -1,33 +1,65 @@
 import { getAuth, signOut } from "firebase/auth";
 import app from "../Firebase";
 import { useNavigate } from 'react-router';
+import { getDatabase } from "firebase/database";
+import { useState } from "react";
 
 const auth = getAuth(app)
+const database = getDatabase(app);
+
 const Dashboard = () => {
+    const [input, setInput] = useState('')
+    const [name, setName] = useState([])
+
+
     const navigate = useNavigate()
     const handleSignOut = (e) => {
 
         signOut(auth).then((value) => {
             navigate('/')
-
         }).catch((error) => {
             console.log(error)
-
         });
     }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newtod = {
+            title: input
+        }
+        setName(prev => ([...prev, newtod]))
+    }
+
+
+    console.log(name);
+
     return (
         <div className="h-screen w-full bg-amber-100 ">
             <div className="h-[10vh] w-full bg-amber-300">
                 <button onClick={handleSignOut}>Sign Out</button>
             </div>
 
-            <div className="h-[90%] w-full bg-amber-500 flex ">
-                <div className="bg-red-300 h-full w-[20%]">
+            <div className="h-[90%] w-full  flex ">
+                <div className=" h-full w-[20%]">
                 </div>
-                <div className="bg-red-500 h-full w-[60%]">
-                 
-                        <input type="text" className="w-50 h-10 bg-amber-700"/>
-                    
+                <div className=" h-full w-[60%] flex flex-col gap-5 ">
+                    <form onSubmit={handleSubmit}>
+                        <div className="w-full h-10 flex justify-center items-center content-center ">
+                            <input type="text" className="w-50 h-10  rounded-l-3xl p-2 border" onChange={(e) => setInput(e.target.value)
+                            } />
+                            <button className="bg-amber-200 h-10 w-20 rounded-r-3xl" >Add Todo</button>
+                        </div>
+                    </form>
+
+                    <div className="todo-div h-[90%] w-[100%] bg-amber-400 p-5 gap-2  flex flex-col">
+                        {name.map((e, i) => (
+                                e.title === '' ? '': 
+                                <div key={i} className="fuck">{e.title}</div>
+                        ))}
+                    </div>
+
                 </div>
                 <div className="bg-red-300 h-full w-[20%]"></div>
             </div>
