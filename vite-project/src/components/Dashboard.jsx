f6e2ee30657337c5cbb741073f6584317214fc38
avermx <aniket125verma@gmail.com>
2025-09-05 23:33:59 +0530

    int

--- vite-project/src/components/Dashboard.jsx
+++ vite-project/src/components/Dashboard.jsx
@@ -105,8 +105,8 @@ const Dashboard = () => {
 
     };
 
-    const handleDelete = () => {
-
+    const handleDelete = (id) => {
+        setName(name.filter((e) => (e.id !== id)))
     }
 
     useEffect(() => {
@@ -173,11 +173,10 @@ const Dashboard = () => {
                                         {data?.title}
                                     </h1>
                                 </div>
-                                <div className="pr-1">
+                                <div className="pr-1 " onClick={() => handleDelete(data.id)}>
                                     <svg class="w-5 h-5 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                     </svg>
-
                                 </div>
                             </div>
                         ))}
