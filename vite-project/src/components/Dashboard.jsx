6d79e720edef41d155d53e7d09654e57f210834f
avermx <aniket125verma@gmail.com>
2025-09-06 23:15:38 +0530

    init

--- vite-project/src/components/Dashboard.jsx
+++ vite-project/src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ import {
 } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import app from "../Firebase";
-import { useNavigate } from "react-router";
+import { data, useNavigate } from "react-router";
 
 import { useEffect, useState } from "react";
 
@@ -39,19 +39,13 @@ const Dashboard = () => {
             if (input === "") {
                 return;
             } else {
-                const newtodo = {
-                    id: name.length + 1,
-                    title: input,
-                    completed: false,
-                };
-
                 await addDoc(collection(db, "users"), {
-                    id: name.length + 1,
                     title: input,
                     completed: false,
+                    id: name.length + 1,
                 });
+                setName((prev) => [...prev, { title: input, completed: false, id: name.length + 1 }]);
                 setInput("");
-                setName((prev) => [...prev, newtodo]);
             }
         } catch (error) {
             console.log(error);
@@ -61,7 +55,7 @@ const Dashboard = () => {
     const checkBox = (id) => {
         setName(
             name.map((title) => {
-                console.log(title);
+
 
                 if (title?.id === id) {
                     return {
@@ -110,14 +104,14 @@ const Dashboard = () => {
     }
 
     useEffect(() => {
-        // const querySnapshot = async () => {
-        //     const hello = await getDocs(collection(db, "users"))
-        //     hello.forEach((doc) => {
-        //         setName([...doc.data(),doc.id])
-        //         return
-        //     });
-        // }
-        // querySnapshot()
+        const querySnapshot = async () => {
+            const hello = await getDocs(collection(db, "users"))
+            hello.forEach((doc) => {
+                setName((prev) => {...prev, id: doc.data()})
+
+        });
+}
+querySnapshot()
         // const hello = () => {
         //     const unsub = onSnapshot(collection(db, "users"), (doc) => {
         //         doc.forEach((e) => {
@@ -129,75 +123,77 @@ const Dashboard = () => {
         // hello()
     }, []);
 
-    return (
-        <div className="h-screen w-full bg-amber-100 ">
-            <div className="h-[10vh] w-full bg-amber-300">
-                <button onClick={handleSignOut}>Sign Out</button>
-            </div>
+console.log(name)
 
-            <div className="h-[90%] w-full  flex ">
-                <div className=" h-full w-[20%]"></div>
-                <div className=" h-full w-[60%] flex flex-col gap-5 ">
-                    <form onSubmit={handleSubmit}>
-                        <div className="w-full h-10 flex justify-center items-center content-center ">
-                            <input
-                                type="text"
-                                className="w-50 h-10  rounded-l-3xl p-2 border"
-                                value={input}
-                                onChange={(e) => setInput(e.target.value)}
-                            />
-                            <button className="bg-amber-200 h-10 w-20 rounded-r-3xl">
-                                Add Todo
-                            </button>
-                        </div>
-                    </form>
+return (
+    <div className="h-screen w-full bg-amber-100 ">
+        <div className="h-[10vh] w-full bg-amber-300">
+            <button onClick={handleSignOut}>Sign Out</button>
+        </div>
 
-                    <div className="todo-div  w-[100%] bg-amber-400 p-5 gap-2 flex flex-col items-center">
-                        {name.map((data, i) => (
-                            <div className="w-[50%] h-[4vh] bg-amber-50 flex items-center">
-                                <div className="pl-2">
-                                    <input
-                                        type="checkbox"
-                                        onChange={() => checkBox(data?.id)}
-                                        checked={data?.completed}
-                                    />
-                                </div>
-                                <div
-                                    className="flex justify-center items-center w-full"
-                                    onClick={() => TodoClicked(data?.id, data?.title)}
-                                >
-                                    <h1
-                                        className={`text-center ${data?.completed ? "line-through" : ""
-                                            }`}
-                                    >
-                                        {data?.title}
-                                    </h1>
-                                </div>
-                                <div className="pr-1 " onClick={() => handleDelete(data.id)}>
-                                    <svg class="w-5 h-5 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                                    </svg>
-                                </div>
-                            </div>
-                        ))}
+        <div className="h-[90%] w-full  flex ">
+            <div className=" h-full w-[20%]"></div>
+            <div className=" h-full w-[60%] flex flex-col gap-5 ">
+                <form onSubmit={handleSubmit}>
+                    <div className="w-full h-10 flex justify-center items-center content-center ">
+                        <input
+                            type="text"
+                            className="w-50 h-10  rounded-l-3xl p-2 border"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                        />
+                        <button className="bg-amber-200 h-10 w-20 rounded-r-3xl">
+                            Add Todo
+                        </button>
                     </div>
-                </div>
-                <div className="bg-red-300 h-full w-[20%]">
-                    <form onSubmit={handleSubmitUpdate}>
-                        <div className="h-full w-full bg-amber-200 text-center ">
-                            <input
-                                value={updateInp}
-                                type="text"
-                                className="border"
-                                onChange={(e) => setUpdateInp(e.target.value)}
-                            />
-                            <button disabled={updateInp === ''} className="bg-amber-950">Update</button>
+                </form>
+
+                <div className="todo-div  w-[100%] bg-amber-400 p-5 gap-2 flex flex-col items-center">
+                    {name.map((data, i) => (
+                        <div className="w-[50%] h-[4vh] bg-amber-50 flex items-center">
+                            <div className="pl-2">
+                                <input
+                                    type="checkbox"
+                                    onChange={() => checkBox(data?.id)}
+                                    checked={data?.completed}
+                                />
+                            </div>
+                            <div
+                                className="flex justify-center items-center w-full"
+                                onClick={() => TodoClicked(data?.id, data?.title)}
+                            >
+                                <h1
+                                    className={`text-center ${data?.completed ? "line-through" : ""
+                                        }`}
+                                >
+                                    {data?.title}
+                                </h1>
+                            </div>
+                            <div className="pr-1 " onClick={() => handleDelete(data.id)}>
+                                <svg class="w-5 h-5 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                </svg>
+                            </div>
                         </div>
-                    </form>
+                    ))}
                 </div>
+            </div>
+            <div className="bg-red-300 h-full w-[20%]">
+                <form onSubmit={handleSubmitUpdate}>
+                    <div className="h-full w-full bg-amber-200 text-center ">
+                        <input
+                            value={updateInp}
+                            type="text"
+                            className="border"
+                            onChange={(e) => setUpdateInp(e.target.value)}
+                        />
+                        <button disabled={updateInp === ''} className="bg-amber-950">Update</button>
+                    </div>
+                </form>
             </div>
         </div>
-    );
+    </div>
+);
 };
 
 export default Dashboard;
