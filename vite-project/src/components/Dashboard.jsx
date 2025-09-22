963ab307354d01aacc092008347a317839608048
avermx <aniket125verma@gmail.com>
2025-09-22 23:35:09 +0530

    init

--- vite-project/src/components/Dashboard.jsx
+++ vite-project/src/components/Dashboard.jsx
@@ -47,11 +47,12 @@ const auth = getAuth(app);
 
 const Dashboard = () => {
     const [input, setInput] = useState();
+    const [inpDes, setInpDes] = useState();
     const [name, setName] = useState([]);
     const [updateInp, setUpdateInp] = useState();
     const [editId, setEditId] = useState("");
     const [uid, setUid] = useState();
-
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -90,20 +91,26 @@ const Dashboard = () => {
             if (input === "") {
                 return;
             } else {
-                await addDoc(collection(db, `users/${uid}/todo`), {
+                const docRef = await addDoc(collection(db, `users/${uid}/todo`), {
                     title: input,
+                    description: inpDes,
                     completed: false,
-                    id: name.length + 1,
+                  
                 });
                 setName((prev) => [
                     ...prev,
-                    { title: input, completed: false, id: name.length + 1 },
+                    { title: input, description: inpDes, completed: false, id: docRef.id },
                 ]);
-                setInput("");
+
             }
         } catch (error) {
             console.log(error);
+
         }
+        setInput("");
+        setInpDes("");
+        setIsDialogOpen(false)
+
     };
 
     const checkBox = (id) => {
@@ -140,7 +147,6 @@ const Dashboard = () => {
             name.map((e) => {
                 if (e.id === editId) {
                     updateDoc(doc(db, `users/${uid}/todo`, e.id), {
-
                         ...e,
                         title: updateInp,
                     });
@@ -157,10 +163,20 @@ const Dashboard = () => {
         setUpdateInp("");
     };
 
-    const handleDelete = (id) => {
+    const handleDelete = (id, e) => {
+
         setName(name.filter((e) => e.id !== id));
         deleteDoc(doc(db, `users/${uid}/todo`, id));
     };
+
+    const ResetForm = (e) => {
+
+        [{
+            title: setInput(''),
+            description: setInpDes('')
+        }]
+
+    }
 
     return (
         <>
@@ -169,38 +185,47 @@ const Dashboard = () => {
                 <div className="h-[10vh] w-full ">
                     <button onClick={handleSignOut}>Sign Out</button>
                 </div>
-
                 <div className="h-[90%] w-full  flex ">
-                    <div className=" h-full w-[20%] "> </div>
+                    <div className=" h-full w-[20%] "></div>
                     <div className=" h-full w-[60%] flex flex-col gap-5 justify-center">
                         <div className="w-[100%] flex items-end justify-end">
-                            <Dialog className="b">
-                                <form>
-                                    <DialogTrigger asChild c>
-                                        <Button variant="outline">Add Todo</Button>
-                                    </DialogTrigger>
-                                    <DialogContent className="sm:max-w-[425px] bg-background dark text-foreground">
+                            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                                <DialogTrigger asChild >
+                                    <Button
+                                        onClick={() => {
+                                            ResetForm()
+                                        }}
+                                    >
+                                        Add Todo
+                                    </Button>
+                                </DialogTrigger>
+                                <DialogContent className="sm:max-w-[425px] bg-background dark text-foreground ">
+                                    <form onSubmit={handleSubmit} className="w-full h-[100%] flex flex-col gap-5">
                                         <DialogHeader>
                                             <DialogTitle>Create New Todo</DialogTitle>
                                         </DialogHeader>
                                         <div className="grid gap-4">
                                             <div className="grid gap-3">
                                                 <Label htmlFor="name-1">Title</Label>
-                                                <Input placeholder="Enter todo title..." />
+                                                <Input placeholder="Enter todo title..." value={input}
+                                                    onChange={(e) => setInput(e.target.value)} />
                                             </div>
                                             <div className="grid gap-3">
                                                 <Label >Description</Label>
-                                                <Input placeholder="Enter todo description..." />
+                                                <Input placeholder="Enter todo description..."
+                                                    value={inpDes}
+                                                    onChange={(e) => setInpDes(e.target.value)}
+                                                />
                                             </div>
                                         </div>
                                         <DialogFooter>
                                             <DialogClose asChild>
-                                                <Button variant="outline">Cancel</Button>
+                                                <Button variant="outline" onClick={ResetForm}>Cancel</Button>
                                             </DialogClose>
                                             <Button type="submit">Save changes</Button>
                                         </DialogFooter>
-                                    </DialogContent>
-                                </form>
+                                    </form>
+                                </DialogContent>
                             </Dialog>
                         </div>
                         {/* <form onSubmit={handleSubmit}>
@@ -217,22 +242,24 @@ const Dashboard = () => {
                             </div>
                         </form> */}
 
-                        <ScrollArea className={'h-[88%] '}>
-                            <div className=" bg-background p-[2%] gap-2 flex flex-col items-center text-center">
+                        <ScrollArea className={'h-[80%]'}>
+                            <div className=" bg-background  gap-2 flex flex-col items-center text-center">
                                 {name.map((data, i) => (
-                                    <Card className={'w-[75%] dark text-center'}>
-                                        <CardHeader className={'flex items-center justify-between text-center gap-[2%] w-[100%]'} onClick={() => TodoClicked(data?.id, data?.title)}>
-                                            <CardTitle ><Input
-                                                type="checkbox"
-                                                onChange={() => checkBox(data?.id)}
-                                                checked={data?.completed}
-                                            /></CardTitle>
-                                            <CardTitle onClick={() => TodoClicked(data?.id, data?.title)}>
-                                                <h1 className={`text-center whitespace-normal break-all text-[1.5rem] ${data?.completed ? "line-through" : ""}`}
-                                                >{data?.title}</h1>
-                                            </CardTitle>
-                                            <CardTitle onClick={() => handleDelete(data.id)}>
-                                                <Button variant={'ghost'}>
+                                    <Card className={'w-[75%] dark  gap-[2%]'}>
+                                        <CardHeader className={'flex items-center  gap-[1%] '} onClick={() => TodoClicked(data?.id, data?.title)}>
+                                            <div className="flex items-center gap-[2%] w-full">
+                                                <CardTitle><Input
+                                                    type="checkbox"
+                                                    onChange={() => checkBox(data?.id)}
+                                                    checked={data?.completed}
+                                                /></CardTitle>
+                                                <CardTitle onClick={() => TodoClicked(data?.id, data?.title)}>
+                                                    <h1 className={`text-center whitespace-normal break-all text-[1.2rem] ${data?.completed ? "line-through" : ""}`}
+                                                    >{data?.title}</h1>
+                                                </CardTitle>
+                                            </div>
+                                            <CardTitle >
+                                                <Button variant={'ghost'} onClick={() => handleDelete(data.id)}>
                                                     <svg
                                                         class="w-10 h-10 text-red-700 "
                                                         aria-hidden="true"
@@ -253,6 +280,9 @@ const Dashboard = () => {
                                                 </Button>
                                             </CardTitle>
                                         </CardHeader>
+                                        <CardDescription >
+                                            <p className={`w-full flex items-start justify-start pl-12 ${data?.completed ? "line-through" : ""}`}>{data?.description}</p>
+                                        </CardDescription>
                                     </Card>
 
                                     // <div className="w-[50%] h-fit bg-amber-50 flex items-center">
@@ -302,7 +332,7 @@ const Dashboard = () => {
                 </div>
 
 
-            </div>
+            </div >
         </>
     );
 };
